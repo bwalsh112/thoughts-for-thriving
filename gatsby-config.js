3daabdf5a0f17fd1c309ac4d5070998d445a8100
bwalsh11 <walsh.swc@gmail.com>
2021-06-06 16:24:42 -0700

    updating site look and feel with colors and correct links to places

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Thoughts for Thriving',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'A blog/community where you can oin me in my journey toward restoring my thyroid and overall health',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
